90f357bd221274b8c13cf753ef1d0febed91cf26
trdmonkey <trdmonkey218@gmail.com>
2026-01-15 21:54:57 -0500

    avance responsive version desktop front at home

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,7 +4,7 @@ import ArrowDown from '@/assets/icons/arrow-down.svg';
 
 export const HeroSection = () => {
   return (
-  <div className='py-32' >
+  <div className="py-32 md:py-48 lg:py-60">
     <div className="container">
       <div className="flex flex-col items-center" >
         <Image 
@@ -14,23 +14,27 @@ export const HeroSection = () => {
         />
         <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-3 rounded-full">
           <div className="bg-green-500 size-2.5 rounded-full" ></div>
-          <div className="text-sm font-medium" >Disponible para nuevos proyectos</div>
+          <div className="text-sm font-medium" >
+            Disponible para nuevos proyectos
+          </div>
         </div>
       </div>
-      <h1 className="font-serif text-3xl text-center mt-8 tracking-wider">
-        Building Excepcional User Experiences
-      </h1>
-      <p className="mt-4 text-center text-white/60"> 
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quae laboriosam magni sapiente suscipit ipsam consequuntur! Fugiat ab neque, commodi at quia quod inventore natus animi quis aliquam eum nulla! 
-      </p>
-      <div className="flex flex-col items-center mt-4">
+      <div className="max-w-lg mx-auto">
+        <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wider">
+          Building Excepcional User Experiences
+        </h1>
+        <p className="mt-4 text-center text-white/60 md:text-lg"> 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quae laboriosam magni sapiente suscipit ipsam consequuntur! Fugiat ab neque, commodi at quia quod inventore natus animi quis aliquam eum nulla! 
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
         <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-full">
           <span className="font-semibold" >Explora nuestro trabajo</span>
           <ArrowDown className="size-4" />
         </button>
-        <button>
+        <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-full">
           <span>👋</span>
-          <span>Let's Connect</span>
+          <span className="font-semibold" >Let's Connect</span>
         </button>
       </div>
     </div>
