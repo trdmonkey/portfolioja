3e748c772c7812c4ca62c708365ffe06361fb6e3
1magnumsc <1magnumsc@gmail.com>
2026-01-25 10:05:47 -0500

    agregando animaciones parte 1 al Hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,10 +2,13 @@ import memojiImage from '@/assets/images/logo6.png';
 import Image from 'next/image';
 import ArrowDown from '@/assets/icons/arrow-down.svg';
 import grainImage from '@/assets/images/fondo18.jpg';
+import StarIcon from '@/assets/icons/star.svg';
+import { HeroOrbit } from '@/components/HeroOrbit';
+
 
 export const HeroSection = () => {
   return (
-  <div className="py-32 md:py-48 lg:py-60 relative z-0">
+  <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
 
     <div 
       className="absolute inset-0 -z-30 opacity-15" 
@@ -18,6 +21,12 @@ export const HeroSection = () => {
     <div className="size-[820px] absolute inset-0 border-2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full border-emerald-300/5 shadow-[0_0_80px_inset] shadow-emerald-300/5"></div>
     <div className="size-[1020px] absolute inset-0 border-2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full border-emerald-300/5 shadow-[0_0_80px_inset] shadow-emerald-300/5"></div>
     <div className="size-[1220px] absolute inset-0 border-2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full border-emerald-300/5 shadow-[0_0_80px_inset] shadow-emerald-300/5"></div>
+      <HeroOrbit size={800} rotation={-72} >
+          <StarIcon className="size-28 text-cyan-100" />
+      </HeroOrbit>
+      <HeroOrbit size={550} rotation={-12}>
+          <StarIcon className="size-12 text-cyan-100" />
+      </HeroOrbit>
 
     <div className="container">
       <div className="flex flex-col items-center" >
