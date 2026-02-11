5d2c0892dd90fe4d1f011151b52c9abd885941bc
trdmonkey <trdmonkey218@gmail.com>
2026-02-11 15:56:16 -0500

    mejora responsive al MD (1080x720)

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -57,21 +57,21 @@ export const ProjectsSection = () => {
           </p>
         </div>
 
-        <h2 className="font-serif text-3xl text-center mt-6">
+        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
           Featured Projects
         </h2>
-        <p className="text-center text-white/60 mt-4">
+        <p className="text-center md:text-lg text-white/60 mt-4 max-w-md mx-auto">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
           reprehenderit.
         </p>
-        <div className="mt-10 flex flex-col gap-12">
+        <div className="mt-10 md:mt-20 flex flex-col gap-12">
           {portfolioProjects.map((project) => (
             <div
               key={project.title}
-              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8" /* p-8 para que la imagen salga completa y con padding-bottom */
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none" /* p-8 para que la imagen salga completa y con padding-bottom */
             >
               <div
-                className="absolute inset-0 -z-10 opacity-5"
+                className="absolute inset-0 -z-10 opacity-10 bg-cover bg-center"
                 style={{
                   backgroundImage: `url(${grainImage.src})`,
                 }}
@@ -81,18 +81,18 @@ export const ProjectsSection = () => {
                 <span>&bull;</span>
                 <span>{project.year}</span>
               </div>
-              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
-              <hr className="border-t-2 border-white/5 mt-4" />
-              <ul className="flex flex-col gap-2 mt-4">
+              <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">{project.title}</h3>
+              <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+              <ul className="flex flex-col gap-2 mt-4 md:mt-5">
                 {project.results.map((result) => (
-                  <li className="flex gap-2 text-sm text-white/50">
-                    <CheckCircleIcon className="size-5" />
+                  <li className="flex gap-2 text-sm md:text-base text-white/50">
+                    <CheckCircleIcon className="size-5 md:size-6"/>
                     <span>{result.title}</span>
                   </li>
                 ))}
               </ul>
               <a href={project.link}>
-                <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
                   <span>View Live Site</span>
                   <ArrowUpRightIcon className="size-4" />
                 </button>
@@ -100,7 +100,7 @@ export const ProjectsSection = () => {
               <Image
                 src={project.image}
                 alt={project.title}
-                className="mt-8 -mb-4"
+                className="mt-8 -mb-4 md:-mb-0 rounded-lg object-cover" /* -mb-4 para que la imagen salga completa y con padding-bottom */
               />{" "}
               {/* el -mb-4 quita padding-bottom al card */}
             </div>
