fb0f8f91a99aa0721fe6e619633a57e28db096e5
trdmonkey <trdmonkey218@gmail.com>
2026-05-13 13:36:08 -0500

    preparando siguiente secction: Tape - Part 1

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,31 @@
+import StarIcon from '@/assets/icons/star.svg';
+
+const words = [
+  "Performant", 
+  "Accessible", 
+  "Secure", 
+  "Interactive", 
+  "Scalable", 
+  "User Friendly", 
+  "Maintainable", 
+  "Search Optimized", 
+  "Usable", 
+  "Reliable", 
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div className="py-16">
+      <div className="bg-gradient-to-r from-emerald-300 bg-gray-800" >
+        <div className="flex gap-4" >
+          {words.map(word => (
+            <div key={word} className="inline-flex gap-4">
+              <span>{word}</span>
+              <StarIcon className="size-6" />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div> 
+  );
 };
