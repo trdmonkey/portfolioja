637e7dc7181ad64bb1124729c6430dbc8efcac51
trdmonkey <trdmonkey218@gmail.com>
2026-05-23 09:55:08 -0500

    creacion de cinta diagonal, para dividir 2 secciones

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -7,6 +7,7 @@ const words = [
   "Interactive", 
   "Scalable", 
   "User Friendly", 
+  "Responsive", 
   "Maintainable", 
   "Search Optimized", 
   "Usable", 
@@ -15,15 +16,17 @@ const words = [
 
 export const TapeSection = () => {
   return (
-    <div className="py-16">
-      <div className="bg-gradient-to-r from-emerald-300 bg-gray-800" >
-        <div className="flex gap-4" >
-          {words.map(word => (
-            <div key={word} className="inline-flex gap-4">
-              <span>{word}</span>
-              <StarIcon className="size-6" />
-            </div>
-          ))}
+    <div className="py-16 lg:py-12 overflow-x-clip">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-700 -rotate-3 -mx-1">
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-4 py-3">
+            {words.map(word => (
+              <div key={word} className="inline-flex gap-4 items-center">
+                <span className="text-gray-900 uppercase font-extrabold text-sm" >{word}</span>
+                <StarIcon className="size-6 text-gray-100 -rotate-12 " />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div> 
