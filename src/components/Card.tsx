d546e964ce261244fccc71a66db7a929556fba6b
trdmonkey <trdmonkey218@gmail.com>
2026-05-25 11:27:25 -0500

    Fix: cards clientes Part 2

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,25 @@
-/* export const Card = () => {
-    return (
+import grainImage from '@/assets/images/grain.jpg';
+import { PropsWithChildren } from 'react';
 
-    )
-} */
+import { twMerge } from 'tailwind-merge';
+
+export const Card = ({ 
+    className, 
+    children, 
+}: PropsWithChildren<{
+    className?: string
+}>) => {
+    return (
+        <div
+            className={twMerge("bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none p-6", className)} /* p-8 para que la imagen salga completa y con padding-bottom */
+            >   
+            <div
+            className="absolute inset-0 -z-10 opacity-10 bg-cover bg-center"
+            style={{
+                backgroundImage: `url(${grainImage.src})`,
+            }}
+            ></div>
+            {children}
+        </div>
+    );
+};
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -8,6 +8,7 @@ import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 import grainImage from "@/assets/images/fondo1.jpg";
 import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
 
 const portfolioProjects = [
   {
@@ -68,16 +69,16 @@ export const ProjectsSection = () => {
         </p> */}
         <div className="mt-10 md:mt-20 flex flex-col gap-12">
           {portfolioProjects.map((project) => (
-            <div
+            <Card
               key={project.title}
-              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:pt-12 lg:px-14 after:pointer-events-none" /* p-8 para que la imagen salga completa y con padding-bottom */
+              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-12 lg:px-14" /* p-8 para que la imagen salga completa y con padding-bottom */
             >
-              <div
+              {/* <div
                 className="absolute inset-0 -z-10 opacity-10 bg-cover bg-center"
                 style={{
                   backgroundImage: `url(${grainImage.src})`,
                 }}
-              ></div>
+              ></div> */}
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-12" >
                   <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
@@ -112,7 +113,7 @@ export const ProjectsSection = () => {
                   />
                 </div>
               </div>
-            </div>
+            </Card>
           ))}
         </div>
       </div>
