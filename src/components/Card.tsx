01551f1d7a8fed446c80831dd51a4f9b6b3718e2
trdmonkey <trdmonkey218@gmail.com>
2026-05-25 19:46:44 -0500

    Fix: section clientes Part 3. Agregando diseño responsive y ajuste a los padding

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -14,7 +14,7 @@ export const Card = ({
             className={twMerge("bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none p-6", className)} /* p-8 para que la imagen salga completa y con padding-bottom */
             >   
             <div
-            className="absolute inset-0 -z-10 opacity-20 bg-cover bg-center"
+            className="absolute inset-0 -z-10 opacity-10 bg-cover bg-center"
             style={{
                 backgroundImage: `url(${grainImage.src})`,
             }}
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -5,7 +5,7 @@ import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
 import { SectionHeader } from "@/components/SectionHeader";
 import Image from "next/image";
-import grainImage from '@/assets/images/grain.jpg';
+import grainImage from "@/assets/images/grain.jpg";
 import { Card } from "@/components/Card";
 
 const testimonials = [
@@ -43,28 +43,36 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-    <div className="py-16" >
+    <div className="py-16">
       <div className="container">
-        <SectionHeader eyebrow="Happy Clients" title="What Clients Say about Me" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's." />
-        <div className="mt-16" >
-          {testimonials.map((testimonial) => (
-            <Card key={testimonial.name} >
-              <div className="flex gap-4 items-center" >
-                <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full">
-                  <Image 
-                    src={testimonial.avatar} 
-                    alt={testimonial.name} 
-                    className="max-h-full" 
-                  />
+        <SectionHeader
+          eyebrow="Happy Clients"
+          title="What Clients Say about Me"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's."
+        />
+        <div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent_0%,black_10%,black_90%,transparent_100%)]">
+          <div className="flex gap-8 flex-none">
+            {testimonials.map((testimonial) => (
+              <Card key={testimonial.name} className="max-w-xs">
+                <div className="flex gap-4 items-center">
+                  <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                    <Image
+                      src={testimonial.avatar}
+                      alt={testimonial.name}
+                      className="max-h-full"
+                    />
+                  </div>
+                  <div>
+                    <div className="font-semibold">{testimonial.name}</div>
+                    <div className="text-sm text-white/40">
+                      {testimonial.position}
+                    </div>
+                  </div>
                 </div>
-                <div>
-                  <div className="font-semibold" >{testimonial.name}</div>
-                  <div className="text-sm text-white/40" >{testimonial.position}</div>
-                </div>
-              </div>
-              <p className="mt-4 text-sm" >{testimonial.text}</p>
-            </Card>
-          ))}
+                <p className="mt-4 text-sm">{testimonial.text}</p>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
