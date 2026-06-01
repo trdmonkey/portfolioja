9331275ac39f99fec561aaab4bf9d7680d558758
trdmonkey <trdmonkey218@gmail.com>
2026-06-01 11:31:23 -0500

    Fix: Section Contact: crear modulo, componente, agregar diseño, responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
 import { TestimonialsSection } from "@/sections/Testimonials";
 import { AboutSection } from "@/sections/About";
+import { ContactSection } from "@/sections/Contact";
+import { Footer } from "@/sections/Footer";
 
 export default function Home() {
   return (
@@ -15,6 +17,8 @@ export default function Home() {
       <TapeSection />
       <TestimonialsSection />
       <AboutSection />
+      <ContactSection />
+      <Footer />
     </div>
   );
 }
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,36 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
+
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <div className="py-16 pt-4 lg:py-16 lg:pt-4">
+      <div className="container">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0">
+          <div
+            className="absolute inset-0 opacity-15 -z-10"
+            style={{
+              backgroundImage: `url(${grainImage.src})`,
+            }}
+          ></div>
+          <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
+            <div>
+              <h2 className="font-serif text-2xl md:text-3xl">
+                Creemos juntos algo increible
+              </h2>
+              <p className="text-sm md:text-base mt-2">
+                ¿Listo para dar vida a tu próximo proyecto? Conectemos y
+                hablemos sobre cómo puedo ayudarte a alcanzar tus objetivos.
+              </p>
+            </div>
+            <div>
+              <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
+                <span className="font-semibold">Contactanos</span>
+                <ArrowUpRightIcon className="size-4" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
