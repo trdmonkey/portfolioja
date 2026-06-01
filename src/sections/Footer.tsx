d29dcb9cf84d214deee370ddb7d857dbd45139cc
trdmonkey <trdmonkey218@gmail.com>
2026-06-01 16:17:02 -0500

    Fix: Section Footer: agregando iconos svg y centrando divs

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,40 @@
+import ArrowUpRightIcon from '@/assets/icons/youtube.svg';
+
+const footerLinks = [
+  {
+    title: "YouTube",
+    href: "",
+  },
+  {
+    title: "Twitter",
+    href: "",
+  },
+  {
+    title: "Instragram",
+    href: "",
+  },
+  {
+    title: "LinkedIn",
+    href: "",
+  },
+];
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div className='border-t border-white/15 py-6 text-sm flex flex-col items-center' >
+          <div>&copy; 2026. Todos los derechos reservados.</div>
+          <nav>
+            {footerLinks.map((link) => (
+              <a href="#" key={link.title}>
+                {link.title}
+                <ArrowUpRightIcon className="size-5" />
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 };
