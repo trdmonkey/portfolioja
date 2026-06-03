dc9137a4e9f23391d4960945f67edf15d18d0ce0
trdmonkey <trdmonkey218@gmail.com>
2026-06-03 17:28:59 -0500

    Animamos el TAPE o cinta bajo el modulo Projects

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,17 +1,18 @@
-import StarIcon from '@/assets/icons/bolt.svg';
+import StarIcon from "@/assets/icons/bolt.svg";
+import { Fragment } from "react";
 
 const words = [
-  "Performant", 
-  "Accessible", 
-  "Secure", 
-  "Interactive", 
-  "Scalable", 
-  "User Friendly", 
-  "Responsive", 
-  "Maintainable", 
-  "Search Optimized", 
-  "Usable", 
-  "Reliable", 
+  "Performant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "User Friendly",
+  "Responsive",
+  "Maintainable",
+  "Search Optimized",
+  "Usable",
+  "Reliable",
 ];
 
 export const TapeSection = () => {
@@ -19,16 +20,22 @@ export const TapeSection = () => {
     <div className="py-16 lg:py-12 overflow-x-clip">
       <div className="bg-gradient-to-r from-emerald-300 to-sky-700 -rotate-3 -mx-1">
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex flex-none gap-4 py-3">
-            {words.map(word => (
-              <div key={word} className="inline-flex gap-4 items-center">
-                <span className="text-gray-900 uppercase font-extrabold text-sm" >{word}</span>
-                <StarIcon className="size-6 text-gray-100 -rotate-12 " />
-              </div>
+          <div className="flex flex-none gap-4 pr-4 py-3 animate-move-left [animation-duration:30s]" >
+            {[...new Array(2)].fill(0).map((_, idx) => (
+              <Fragment key={idx}>
+                {words.map((word) => (
+                  <div key={word} className="inline-flex gap-4 items-center">
+                    <span className="text-gray-900 uppercase font-extrabold text-sm">
+                      {word}
+                    </span>
+                    <StarIcon className="size-6 text-gray-100 -rotate-12 " />
+                  </div>
+                ))}
+              </Fragment>
             ))}
           </div>
         </div>
       </div>
-    </div> 
+    </div>
   );
 };
